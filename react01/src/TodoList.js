c78e99885cfcd92c33e481a3d9e1e9b90a21ed4c
songjiwei <939346832@qq.com>
2018-09-02 23:18:08 +0800

    实现todolist增加删除功能[F

--- react01/src/TodoList.js
+++ react01/src/TodoList.js
@@ -1,11 +1,64 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 
 class TodoList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: '',
+            list: []
+        }
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+
+    handleBtnClick() {
+        this.setState({
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ''
+        })
+    }
+
+    handleItemDelete(index) {
+    	const list = [...this.state.list];
+    	list.splice(index, 1);
+    	this.setState({
+    		list: list
+    	})
+    }
+
 	render() {
 		return (
-			
+			<Fragment>
+				<div>
+					<input 
+                        value={this.state.inputValue}
+                        onChange={this.handleInputChange.bind(this)}
+                    />
+					<button onClick={this.handleBtnClick.bind(this)}>提交</button>
+				</div>
+				<ul>
+                    {
+                        this.state.list.map((item, index) => {
+                            return 	<li 
+                            			key={index} 
+                            			onClick={this.handleItemDelete.bind(this, index)}
+                            		>
+                                        {item}
+                                    </li>
+                        })
+                    }
+				</ul>
+			</Fragment>
 		)
 	}
 }
 
 export default TodoList;
+
+
+
