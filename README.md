ee294d9505a8661e950036c7bb1798356c32b876
songjiwei <939346832@qq.com>
2018-09-04 15:05:48 +0800

    :tada: Initialize Repo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-todolist
+
+react-todolist
--- react01/src/TodoList.js
+++ react01/src/TodoList.js
@@ -1,9 +1,23 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+
+constructor(props) {
+	super(props);
+}
 
 class TodoList extends Component {
 	render() {
 		return (
-			
+			<Fragment>
+				<div>
+					<input />
+					<button>提交</button>
+				</div>
+				<ul>
+					<li>歲月長，衣裳薄</li>
+					<li>歲月長，衣裳薄</li>
+					<li>歲月長，衣裳薄</li>
+				</ul>
+			</Fragment>
 		)
 	}
 }
