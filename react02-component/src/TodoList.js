67a176caf50e16321414c7d50c0cc3b5622cdb6b
songjiwei <939346832@qq.com>
2018-09-09 16:03:25 +0800

    :sparkles:todolist組件化完成

---
+++ react02-component/src/TodoList.js
@@ -0,0 +1,64 @@
+import React, {Component, Fragment} from 'react';
+import './style.css';
+import TodoItem from './TodoItem';
+
+class TodoList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: '',
+            list: []
+        }
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+
+    handleBtnClick() {
+        this.setState({
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ''
+        })
+    }
+
+    handleItemDelete(index) {
+    	const list = [...this.state.list];
+    	list.splice(index, 1);
+    	this.setState({
+    		list: list
+    	})
+    }
+
+	render() {
+		return (
+			<Fragment>
+				<div className="content">
+					<label htmlFor="insertArea">输入内容</label>
+					<input id="insertArea" value={this.state.inputValue} onChange={this.handleInputChange.bind(this)} className="input"/>
+					<button onClick={this.handleBtnClick.bind(this)} className="button">提交</button>
+				</div>
+				<ul>
+					{
+						this.state.list.map((item, index) => {
+							return (
+								<div>
+									<TodoItem 
+										content={item} 
+										index={index}
+										deleteItem={this.handleItemDelete.bind(this)}
+									/>
+								</div>
+							)
+						})
+					}
+				</ul>
+			</Fragment>
+		)
+	}
+}
+
+export default TodoList;
